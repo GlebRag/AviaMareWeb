89982b141801de15f599a8cac37a0361094e8036
Gleb <flareon.game@gmail.com>
2024-11-28 23:03:19 +0300

    Изменения бизнес модели ролей

--- AviaMare/Enums/Users/Role.cs
+++ AviaMare/Enums/Users/Role.cs
@@ -3,10 +3,10 @@
     [Flags]
     public enum Role
     {
-        User = 1,           // x 0000 0001
-        Admin = 2,          // x 0000 0010
-        AnimeModerator = 4, // x 0000 0100
-        AnimeAuthor = 8,    // x 0000 1000
-        SurveysCreatorOrEditor = 16
+        User = 1,                // x 0000 0001
+        Admin = 2,               // x 0000 0010
+        AviaMareModerator = 4, // x 0000 0100
+                                // x 0000 1000 умножаем число на 2, т.е след роль будет числом 8
+        
     }
 }
