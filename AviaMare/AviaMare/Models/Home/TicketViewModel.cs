b51732dab47bb09bdf6393850607a7159d474030
REELYNX Inc <oficial.arkain123@mail.ru>
2024-12-01 15:21:43 +0300

    redacted viewmodels: IndexViewModel, TicketViewModel

---
+++ AviaMare/AviaMare/Models/Home/TicketViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AviaMare.Models.Home
+{
+    public class TicketViewModel
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public string Departure { get; set; }
+        public int IdPlane { get; set; }
+        public int Time { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime TakeOffTime { get; set; }
+        public DateTime LandingTime { get; set; }
+    }
+}
