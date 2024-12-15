c31748704bcd2ed319a8afbdd09c101a86cb148b
Gleb <flareon.game@gmail.com>
2024-12-15 19:38:03 +0300

    ViewModel for creation

--- AviaMare/AviaMare/Controllers/HomeController.cs
+++ AviaMare/AviaMare/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using AviaMare.Models;
 using AviaMare.Models.Home;
 using AviaMare.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AviaMare.Controllers
 {
@@ -26,6 +27,54 @@ namespace AviaMare.Controllers
 
             return View(viewModel);
         }
+
+        //[HttpGet]
+        //public IActionResult CreateTicket()
+        //{
+        //    var viewModel = new TicketCreationViewModel();
+
+        //    viewModel.FilmDirectors = _filmDirectorRepository
+        //        .GetAll()
+        //        .Select(x => new SelectListItem(x.LastName, x.Id.ToString())) // .Select(x => new SelectListItem(x.Name + " " + x.LastName, x.Id.ToString()))
+        //        .ToList();
+
+        //    return View(viewModel);
+        //}
+
+        //[HttpPost]
+        //public IActionResult CreateTicket(TicketCreationViewModel viewModel)
+        //{
+        //    if (_.HasSimilarUrl(viewModel.Url))
+        //    {
+        //        ModelState.AddModelError(
+        //            nameof(MovieCreationViewModel.Url),
+        //            "Такой url уже есть");
+        //    }
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        viewModel.FilmDirectors = _filmDirectorRepository
+        //            .GetAll()
+        //            .Select(x => new SelectListItem(x.LastName, x.Id.ToString())) // .Select(x => new SelectListItem(x.Name + " " + x.LastName, x.Id.ToString()))
+        //            .ToList();
+
+        //        return View(viewModel);
+        //    }
+
+        //    var currentUserId = _authService.GetUserId();
+
+        //    var dataMovie = new MovieData
+        //    {
+        //        Name = viewModel.Name,
+        //        ImageSrc = viewModel.Url,
+        //        //Tags = viewModel.Tags,
+        //    };
+        //    //_moviePosterRepository.Add(dataMovie);
+
+        //    _moviePosterRepository.Create(dataMovie, currentUserId!.Value, viewModel.FilmDirectorId);
+
+        //    return RedirectToAction("AllPosters");
+        //}
 
         public IActionResult Privacy()
         {
---
+++ AviaMare/AviaMare/Models/Home/TicketCreationViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AviaMare.Models.Home
+{
+    public class TicketCreationViewModel
+    {
+        public string Destination { get; set; }
+        public string Departure { get; set; }
+        public int IdPlane { get; set; }
+        public int Time { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime TakeOffTime { get; set; }
+        public DateTime LandingTime { get; set; }
+    }
+}
