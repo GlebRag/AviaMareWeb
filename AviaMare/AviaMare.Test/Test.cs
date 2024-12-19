fe045980598b436fe6095b32ae48424fabeca7d0
Gleb <flareon.game@gmail.com>
2024-12-20 01:43:30 +0300

    + UTest For Contoller

---
+++ AviaMare/AviaMare.Test/Test.cs
@@ -0,0 +1,45 @@
+﻿using AviaMare.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using UnitTestApp.Controllers;
+using Xunit;
+
+namespace AviaMare.Test
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            // Arrange
+            ControllerForUnitTest controller = new ControllerForUnitTest();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello!", result?.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            ControllerForUnitTest controller = new ControllerForUnitTest();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexViewNameEqualIndex()
+        {
+            // Arrange
+            ControllerForUnitTest controller = new ControllerForUnitTest();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.Equal("Index", result?.ViewName);
+        }
+    }
+}
---
+++ AviaMare/AviaMare/Controllers/ControllerForUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestApp.Controllers
+{
+    public class ControllerForUnitTest : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello!";
+            return View("Index");
+        }
+    }
+}
